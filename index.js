07a57addc35d3026c52a235bd9a6abc8ec14bef4
Arya Pawar <arya.pawar@iide.co>
2025-10-14 15:47:20 +0530

    Change pt 1

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/test', (req, res) => {
+    res.send('Hello Arya');
+});
  
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
